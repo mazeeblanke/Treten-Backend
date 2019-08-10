cd34096c56ed845721e5ff0f8734ff94d90c96ed
Mazino Ukah <ewomaukah@yahoo.com>
2019-08-10 13:07:20 +0100

    wip

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Creativeorange\Gravatar\GravatarServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Package Service Providers...
