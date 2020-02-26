835a21a0f109b0869b9dbcbfeb71a2afe7f8cd78
mazino ukah <ewomaukah@yahoo.com>
2020-02-26 09:58:40 +0100

    Fix missing filterable trait in resource material object

--- app/Resource.php
+++ app/Resource.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use App\Traits\Filterable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Resource extends Model
 {
     use SoftDeletes;
+    use Filterable;
 
     protected $fillable = [
       'file',
